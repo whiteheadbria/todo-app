1478541ae282c9a8feaf279a4b28938bd775ca1b
Bria Whitehead <whitehead.bria@protonmail.com>
2022-07-14 13:17:35 -0500

    the counter by in App.js allows for the individual components to be added from parent component. Have to modifty Counter.js to reflect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FirstComponent from './components/learning-examples/FirstComponent'
 import SecondComponent from './components/learning-examples/SecondComponent'
 import ThirdComponent from './components/learning-examples/ThirdComponent'
 import Counter from './components/counter/Counter'
+import './App.css';
 
 import logo from './logo.svg';
 import './App.css';
@@ -11,8 +12,8 @@ class App extends Component  {
   render() {
   return (
     <div className="App">
-    <Counter />
-  
+    <Counter by="1" />
+    <Counter by="2" />
     </div>
   );
 }
