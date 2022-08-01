525eeebf170a2bb58ed2593552768bbe065b352f
Bria Whitehead <whitehead.bria@protonmail.com>
2022-08-01 16:35:16 -0500

    Redoing everything! Found this video for guidance. https://www.youtube.com/watch?v=pCA4qpQDZD8

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import React, { Component } from 'react';
-import FirstComponent from './components/learning-examples/FirstComponent'
-import SecondComponent from './components/learning-examples/SecondComponent'
-import ThirdComponent from './components/learning-examples/ThirdComponent'
-import Counter from './components/counter/Counter'
+import React from 'react';
 import './App.css';
+// Importing Components
 
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form";
+import List from "./components/List";
 
-class App extends Component  {
-  render() {
+function App() {
   return (
     <div className="App">
-    <Counter />
-
+    <header>
+      <h1>Bria's To-Do List</h1>
+      </header>
+      <Form />
     </div>
   );
-}
-}
-
-
-class LearningComponents extends Component {
-  render() {
-    return (
-      <div className="LearningComponents">
-        My Hello World
-        <FirstComponent />
-        <SecondComponent />
-        <ThirdComponent />
-      </div>
-    );
-  }
 }
 
 export default App;
